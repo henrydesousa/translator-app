0bb8074b82c35ec55049875d2117763a0b4c263b
Henry De Sousa <henry.desousa@pearson.com>
2019-04-16 16:06:28 -0600

    adding mongeez integration

---
+++ guesstheverb/src/main/java/org/henry/guesstheverb/configuration/MongoAppConfig.java
@@ -0,0 +1,41 @@
+package org.henry.guesstheverb.configuration;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.mongeez.Mongeez;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class MongoAppConfig {
+
+    @Value("${mongeez.filePath}")
+    private String mongeezFilePath;
+
+    @Value("${spring.data.mongodb.host}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private String mongoPort;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDatabase;
+
+    @Bean
+    public Mongeez mongeez() {
+        Mongeez mongeez = new Mongeez();
+        mongeez.setFile(new ClassPathResource(mongeezFilePath));
+        mongeez.setMongo(new MongoClient(mongoURI()));
+        mongeez.setDbName(mongoDatabase);
+        mongeez.process();
+        return mongeez;
+    }
+    
+    private MongoClientURI mongoURI() {
+        String mongoUri = String.format("mongodb://%s:%s/%s", mongoHost, mongoPort, mongoDatabase);
+        MongoClientURI mongoClientURI = new MongoClientURI(mongoUri);
+        return mongoClientURI;
+    }
+}
